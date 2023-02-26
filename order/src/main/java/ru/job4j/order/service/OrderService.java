d235c2a0a9952b8b1f235e1aca1656479e09cc40
Arcady555 <parfenov7233@gmail.com>
2023-02-26 23:07:15 +0300

    Повествование основанное на хореографии [#458498]-4

--- order/src/main/java/ru/job4j/order/service/OrderService.java
+++ order/src/main/java/ru/job4j/order/service/OrderService.java
@@ -3,13 +3,8 @@ package ru.job4j.order.service;
 import lombok.Data;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.job4j.order.dto.OrderDto;
 import ru.job4j.order.model.Order;
 import ru.job4j.order.model.Status;
@@ -17,11 +12,8 @@ import ru.job4j.order.repository.OrderRepository;
 
 import java.util.Optional;
 
-@EnableKafka
 @Data
 @Service
-@RestController
-@RequestMapping("/order")
 public class OrderService {
     private final OrderRepository orders;
     private final StatusService statuses;
@@ -38,19 +30,16 @@ public class OrderService {
     }
 
     public void saveOut(Order order) {
-        OrderDto orderDto = new OrderDto();
-        orderDto.setId(order.getId());
-        orderDto.setStatus(order.getStatus());
+        OrderDto orderDto = getDtoFromOrder(order);
         orders.save(order);
         kafkaTemplateO.send("preorder", order.getId(), orderDto);
         kafkaTemplateS.send("messengers", order.getId(), order.getStatus().getName());
-
     }
 
-    public void saveIn(Order order) {
+    public void saveIn(ConsumerRecord<Integer, String> record) {
+        Order order = msgFromKitchen(record);
         orders.save(order);
         kafkaTemplateS.send("messengers", order.getId(), order.getStatus().getName());
-
     }
 
     public Order findById(int id) {
@@ -66,23 +55,18 @@ public class OrderService {
         return order;
     }
 
-    @PostMapping("/kitchen")
-    private void sendToKitchen(Integer orderId) {
-        Order order = new Order();
-        order.setId(orderId);
-        Status status = new Status();
-        status.setId(1);
-        status.setName("принят");
-        order.setStatus(status);
-        saveOut(order);
+    private OrderDto getDtoFromOrder(Order order) {
+        OrderDto orderDto = new OrderDto();
+        orderDto.setId(order.getId());
+        orderDto.setStatus(order.getStatus());
+        return orderDto;
     }
 
-    @KafkaListener(topics = "cooked_order")
-    private void msgFromKitchen(ConsumerRecord<Integer, String> record) {
+    private Order msgFromKitchen(ConsumerRecord<Integer, String> record) {
         int id = record.key();
         Order order = findById(id);
         Status status = statuses.findByName(record.value());
         order.setStatus(status);
-        saveIn(order);
+        return order;
     }
 }

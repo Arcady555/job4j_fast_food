07127cb80a55cccc0c2b9d4d459e893a4db701f0
Arcady555 <parfenov7233@gmail.com>
2023-05-18 17:49:37 +0300

    Dish правки-2

--- dish/src/main/java/ru/dish/repository/DishRepository.java
+++ dish/src/main/java/ru/dish/repository/DishRepository.java
@@ -1,16 +1,13 @@
-package ru.job4j.admin.repository;
+package ru.dish.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.job4j.admin.model.Dish;
+import ru.domain.model.Dish;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface DishRepository extends CrudRepository<Dish, Integer> {
     @Override
     List<Dish> findAll();
-
-    Optional<Dish> findByName(String name);
 }
